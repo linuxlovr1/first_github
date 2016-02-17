03bb64322621445420116250a00d752ba63070f0
Parker Smith <linuxlovr1@gmail.com>
2016-02-17 11:33:49 -0700

    still have work to do

---
+++ first_branch.rb
@@ -0,0 +1,11 @@
+puts "first branch"
+puts "what is your favorite color?"
+favorite_colot = gets.strip.downcase
+of favorite_color == "black"
+  puts "you depressed, bro?"
+else
+	puts "nice"
+end
+# still nee to grab user input
+# still need to output more info
+# still need to get more user input
